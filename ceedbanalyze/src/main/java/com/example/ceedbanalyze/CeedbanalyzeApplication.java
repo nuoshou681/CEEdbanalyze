16091c858e5a3f3ce754c23f9342ffe92a92b140
rainli <2977513360@qq.com>
2024-07-23 11:48:39 +0800

    后端，测试

---
+++ ceedbanalyze/src/main/java/com/example/ceedbanalyze/CeedbanalyzeApplication.java
@@ -0,0 +1,13 @@
+package com.example.ceedbanalyze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeedbanalyzeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeedbanalyzeApplication.class, args);
+	}
+
+}
---
+++ ceedbanalyze/src/test/java/com/example/ceedbanalyze/CeedbanalyzeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ceedbanalyze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeedbanalyzeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
