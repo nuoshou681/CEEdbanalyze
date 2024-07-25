64d78df9dba3b0dded7b17d61587907df8e41400
Liao Hui <143698488+foeria@users.noreply.github.com>
2024-07-25 17:55:37 +0800

    前端骨架搭建

---
+++ ceedbanalyze-ui/src/views/analyseview.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="page_main">
+    
+    <top_menu_bar @update:activeMenu = "updateActiveMenu"
+    :activeMenu="activeMenu"/>
+
+  
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  import top_menu_bar from '@/components/top_menu_bar.vue';
+  const activeMenu = ref('analyse');
+  function updateActiveMenu(menu) {
+  activeMenu.value = menu;
+}
+  </script>
+  
+  <style>
+  .nav-item.active {
+    color: #42b983; /* 示例: 激活状态的文本颜色 */
+    font-weight: bold;
+  }
+  .page_main {
+  background-color: azure;
+  width: 1200px;
+  position: relative;
+  left: 150px;
+  }
+  </style>
