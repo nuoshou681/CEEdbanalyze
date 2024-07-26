67e7d5865614ba47e647fbbf516c41196d0f6186
Lenovo <2244021802@qq.com>
2024-07-26 12:36:20 +0800

    测试上传

--- ceedbanalyze/src/main/java/com/example/ceedbanalyze/CeedbanalyzeApplication.java
+++ ceedbanalyze/src/main/java/com/example/ceedbanalyze/CeedbanalyzeApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CeedbanalyzeApplication {
-	// main
+	// main2
 
 	public static void main(String[] args) {
 		SpringApplication.run(CeedbanalyzeApplication.class, args);
