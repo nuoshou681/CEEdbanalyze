f74a3c407e321dbe89d80082f265157a1fe2ea6b
rainli <2977513360@qq.com>
2024-09-08 10:05:10 +0800

    高考小智后端

--- ceedbanalyze/src/main/java/com/example/ceedbanalyze/controller/MajorLayerThreeController.java
+++ ceedbanalyze/src/main/java/com/example/ceedbanalyze/controller/MajorLayerThreeController.java
@@ -19,7 +19,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Objects;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/major")
