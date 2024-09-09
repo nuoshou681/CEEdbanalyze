91ed760ab15fb051ecd748daf394f20fe503988c
闵凡郡 <3487459458@qq.com>
2024-09-09 19:59:42 +0800

    Add files via upload

--- ceedbanalyze/src/main/java/com/example/ceedbanalyze/mapper/RankingMapper.java
+++ ceedbanalyze/src/main/java/com/example/ceedbanalyze/mapper/RankingMapper.java
@@ -3,6 +3,9 @@ package com.example.ceedbanalyze.mapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.List;
+import java.util.Map;
+
 /*
  * @author      潘广成
  * @data        2024/9/8 11:00
@@ -13,4 +16,7 @@ import org.apache.ibatis.annotations.Select;
 public interface RankingMapper {
     @Select("select `rank` from yifenyiduan where score = #{score}")
     Integer getRanking(int score);
+
+    @Select("select `score`, `num` from yifenyiduan")
+    List<Map<String, Object>> getNum();
 }
--- ceedbanalyze/src/main/java/com/example/ceedbanalyze/utils/RankingUtil.java
+++ ceedbanalyze/src/main/java/com/example/ceedbanalyze/utils/RankingUtil.java
@@ -4,6 +4,9 @@ import com.example.ceedbanalyze.mapper.RankingMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Map;
+
 /*
  * @author      潘广成
  * @data        2024/9/8 10:59
@@ -17,5 +20,8 @@ public class RankingUtil {
 
     public Integer getRanking(int score) {
         return rankingMapper.getRanking(score);
+    }
+    public List<Map<String, Object>> getNum() {
+        return rankingMapper.getNum();
     }
 }
